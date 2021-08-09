97bffe0f5d5655ddbf07f1b7a563fe9adfcdfcc7
WaltyMC <88677583+WaltyMC@users.noreply.github.com>
2021-08-09 12:24:01 -0500

    Create Core.php

---
+++ src/WaltyMC/LobbyCore/Core.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace WaltyMC\LobbyCore;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\{Config, TextFormat as TE};
+
+class Core extends PluginBase implements Listener {
+
+
+}
