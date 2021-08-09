a448f76e55a401b843b81bd73c6b8a1ca542314e
WaltyMC <88677583+WaltyMC@users.noreply.github.com>
2021-08-09 12:41:07 -0500

    update Core.php

--- src/WaltyMC/LobbyCore/Core.php
+++ src/WaltyMC/LobbyCore/Core.php
@@ -5,8 +5,14 @@ namespace WaltyMC\LobbyCore;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\utils\{Config, TextFormat as TE};
+use pocketmine\event\player\PlayerJoinEvent;
 
 class Core extends PluginBase implements Listener {
+  
+  public function onEnable(){
+    $this->getLogger()->info("Plugin Enable!");
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
 
 
 }
